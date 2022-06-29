9e6b123e53a8ad21adc8a8e87973ad76fee9a8cd
naoyanaonaoya <naoya.basket.a.i@gmail.com>
2022-06-30 01:09:12 +0900

    add

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,20 @@
 import * as React from "react";
 import Layout from "../components/layout";
 import { StaticImage } from "gatsby-plugin-image";
+import { Helmet } from "react-helmet";
 
 const HomePage = () => {
   return (
     <>
       <Layout pageTitle="Home page">
+        <Helmet>
+          <html lang="en" />
+          <title>indexページ</title>
+          <meta
+            name="google-site-verification"
+            content="-lDcNDARs_FdlEcqxQHVRabe7E4XCf55T21pQRjwD8c"
+          />
+        </Helmet>
         <p>I'm making this by following the Gatsby Tutorial.</p>
         <StaticImage
           src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
