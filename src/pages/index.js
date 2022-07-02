c9c8042c4e7e16f4e5c1a7656953546351ecf43f
naoyanaonaoya <naoya.basket.a.i@gmail.com>
2022-07-03 03:43:13 +0900

    add

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React from "react";
 import Layout from "../components/Layout";
 import { StaticImage } from "gatsby-plugin-image";
 import SEO from "../components/Seo";
